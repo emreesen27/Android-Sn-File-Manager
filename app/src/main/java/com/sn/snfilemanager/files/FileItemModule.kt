feeaf886e8f4d2fa9a1c44a9b6fae5697d0b8f2c
aemreesen27 <emreesen27@gmail.com>
2023-07-26 19:44:31 +0300

    [DEV][file extension and icons added]

--- app/src/main/java/com/sn/snfilemanager/files/FileItemModule.kt
+++ app/src/main/java/com/sn/snfilemanager/files/FileItemModule.kt
@@ -5,7 +5,9 @@ import com.idanatz.oneadapter.external.event_hooks.ClickEventHook
 import com.idanatz.oneadapter.external.modules.ItemModule
 import com.sn.snfilemanager.R
 import com.sn.snfilemanager.databinding.ItemFileBinding
-
+import com.sn.snfilemanager.extensions.gone
+import com.sn.snfilemanager.extensions.loadWithGlide
+import com.sn.snfilemanager.extensions.visible
 
 class FileItemModule(val context: Context) :
     ItemModule<FileModel>() {
@@ -26,11 +28,31 @@ class FileItemModule(val context: Context) :
                 ) else {
                     context.getString(R.string.file_size, model.lastModified, model.size)
                 }
+                updateUIForModel(model, this)
             }
         }
         eventHooks += ClickEventHook<FileModel>().apply {
             onClick { model, _, _ ->
                 onClick?.invoke(model)
+            }
+        }
+    }
+
+    private fun updateUIForModel(model: FileModel, binding: ItemFileBinding) {
+        with(binding) {
+            ivPlay.gone()
+            if (model.isDirectory) {
+                ivFileImage.setImageResource(R.drawable.ic_folder)
+            } else {
+                val iconResId = FileExtension.getIconResourceId(model.extension)
+                if (iconResId != 0) {
+                    ivFileImage.setImageResource(iconResId)
+                } else if (FileExtension.isVideoExtension(model.extension)) {
+                    ivPlay.visible()
+                    ivFileImage.loadWithGlide(model.absolutePath)
+                } else {
+                    ivFileImage.loadWithGlide(model.absolutePath)
+                }
             }
         }
     }
