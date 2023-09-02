bc012b5cdd45f56048b520d1d0af6d530c630122
aemreesen27 <emreesen27@gmail.com>
2023-09-02 13:42:05 +0300

    [DEV][fragment nav result extension added]

---
+++ app/src/main/java/com/sn/snfilemanager/core/extensions/Fragment.kt
@@ -0,0 +1,11 @@
+package com.sn.snfilemanager.core.extensions
+
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+
+fun Fragment.getNavigationResult(key: String = "result") =
+    findNavController().currentBackStackEntry?.savedStateHandle?.getLiveData<String>(key)
+
+fun Fragment.setNavigationResult(result: String, key: String = "result") {
+    findNavController().previousBackStackEntry?.savedStateHandle?.set(key, result)
+}
